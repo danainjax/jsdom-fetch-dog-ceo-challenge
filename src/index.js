c4e9264b31c6806a6d0e14ca21482564c48537c7
danainjax <danainjax@gmail.com>
2021-05-28 14:10:37 -0400

    first challenge completed

--- src/index.js
+++ src/index.js
@@ -1,1 +1,35 @@
-console.log('%c HI', 'color: firebrick')
+
+const imgURL = "https://dog.ceo/api/breeds/image/random/4" 
+const container = document.getElementById('dog-image-container')
+
+//Challenge 1
+fetch(imgURL)
+  .then(response => response.json())
+  .then(images =>  {
+    const imgs = images.message
+    
+    
+    let imgsArray = imgs.map((img) => {
+      let i = `<img src=${img}>`
+      console.log(i);
+      return i
+      console.log(imgsArray);
+    }) 
+
+    imgsArray.forEach(element => {
+      console.log(container)
+        container.innerHTML += element 
+      })
+       
+     
+  })   
+    
+   
+ 
+  //for template literals need .innerHtml +=  otherwise will recognize as a sting not a node
+
+    // fetch the images using the url above ⬆️
+// parse the response as JSON
+// add image elements to the DOM for each🤔 image in the array  
+
+
