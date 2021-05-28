1ed3e5ef844f5451dc182c0a47e708bee6e2131b
danainjax <danainjax@gmail.com>
2021-05-28 12:04:01 -0400

    commit tree 8b1a1cd9f934f8839807fc3077bda77c2a667531

--- src/index.js
+++ src/index.js
@@ -1,1 +1,14 @@
 console.log('%c HI', 'color: firebrick')
+const imgURL = "https://dog.ceo/api/breeds/image/random/4" 
+
+document.addEventListener('DOMContentLoaded', function() {
+  const data = fetch(imgURL).then(response => response.json());
+  data.then(data => console.log(data));
+  })
+ 
+  
+    // fetch the images using the url above ⬆️
+// parse the response as JSON
+// add image elements to the DOM for each🤔 image in the array  
+
+
